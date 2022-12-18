cab1b000a9ce70187b7034a5a921317f31c202a1
njwright92 <njwright92@gmail.com>
2022-12-18 12:13:33 -0800

    js and rem

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$('#loginButton').click('show.bs.modal', function () {
+    $('#loginModal').modal('show');
+});
+
+$('#registerButton').click('show.bs.modal', function () {
+    $('#modalRegisterForm').modal('show');
+});
